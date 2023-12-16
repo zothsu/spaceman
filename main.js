248a758a69b081d5e63f0c447221e953f5aa30c8
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2023-12-16 13:34:02 -0800

    state constants

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 /*----- constants -----*/
+//word bank
+const secretWords = [
+  'comets',
+  'planet',
+  'rocket',
+  'nebula',
+  'galaxy',
+  'cosmic',
+  'meteor',
+  'orbital',
+  'cosmos',
+  'saturn',
+  'quasar',
+];
+
+//select a random word from WORDBANK
+const secretWord = secretWords[Math.floor(Math.random() * (words.length +1))];
+
+
+//initial state of spaceman
+const spacemanImg = document.getElementById('spaceman')
+spacemanImg.innerHTML = '<img src="" alt="image of astronaught in tractor beam">'
+
+
+
+
 
 /*----- app's state (variables) -----*/
+//guessed words
+const guessedLetters = [];
+
+// guesses left
+const guessesLeft = [];
 
 /*----- cached element references -----*/
 
