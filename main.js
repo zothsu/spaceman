5d2d0cf5e78fc325df78c1c1d930dc627e06fe08
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2023-12-16 15:31:54 -0800

    add intro message

--- main.js
+++ main.js
@@ -18,9 +18,11 @@ const secretWords = [
 const secretWord = secretWords[Math.floor(Math.random() * (words.length +1))];
 
 
-//initial state of spaceman
+//initial state of greeting and then spaceman
 const spacemanImg = document.getElementById('spaceman')
 spacemanImg.innerHTML = '<img src="" alt="image of astronaught in tractor beam">'
+const greetingMsg = document.getElementById('spaceman')
+greetingMsg.innerHTML = '<p> Help the spaceman get out of the aliens teleportation beam before the aliens beam up his whole body!! Every wrong guess of the word transmits another part of his body. you only have 6 tries before his body is compleatly gone!!</p>'
 
 
 
