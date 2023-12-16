99c7e04d5c95dd941daf6914b98259865c5a96b4
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2023-12-16 10:39:13 -0800

    add code section headers

---
+++ main.js
@@ -0,0 +1,15 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+init()
+
+// Initialize all state, then call render()
+function init() {
+    //stubbed up
+  };
