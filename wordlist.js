9ed40a232033a895c781abfa8b40221cd31ba7ac
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2023-12-18 07:53:30 -0800

    add word list into object to allow for word hints

--- wordlist.js
+++ wordlist.js
@@ -1,40 +1,42 @@
 const secretWords = [
-  'comets',
-  'planet',
-  'rocket',
-  'nebula',
-  'galaxy',
-  'cosmic',
-  'meteor',
-  'orbital',
-  'cosmos',
-  'saturn',
-  'quasar',
+    'comets',
+    'planet',
+    'rocket',
+    'nebula',
+    'galaxy',
+    'cosmic',
+    'meteor',
+    'orbital',
+    'cosmos',
+    'saturn',
+    'quasar',
 ];
-class WordList:
-    def __init__(self):
-        self.categories = {
-            "space": [
-                {"word": "planet", "hint": "A celestial body that orbits a star"},
-                {"word": "galaxy", "hint": "A large system of stars, gas, and dust held together by gravity"},
-                {"word": "asteroid", "hint": "A small rocky object that orbits the Sun"},
-                {"word": "star", "hint": "A luminous ball of gas that generates energy through nuclear fusion"}
-            ],
-            "food": [
-                {"word": "pizza", "hint": "A popular Italian dish made of round, flattened dough topped with various ingredients"},
-                {"word": "burger", "hint": "A sandwich consisting of a cooked patty of ground meat in a bun"},
-                {"word": "sushi", "hint": "A Japanese dish consisting of vinegared rice topped with various ingredients"},
-                {"word": "pasta", "hint": "An Italian dish made from unleavened dough of wheat flour, water, and sometimes eggs"}
-            ],
-            "games": [
-                {"word": "chess", "hint": "A strategic board game played between two players on a checkered gameboard"},
-                {"word": "monopoly", "hint": "A classic board game where players buy, sell, and trade properties"},
-                {"word": "scrabble", "hint": "A word game in which two to four players score points by placing tiles, each bearing a single letter, onto a game board"},
-                {"word": "jenga", "hint": "A game where players take turns removing one block at a time from a tower and balancing it on top"}
-            ]
-        }
 
-    def get_words(self, category):
-        return self.categories.get(category, [])
 
-word_list = WordList()
+const WORD_LIST = {
+    space: [
+        { word: "astronaut", hint: "A person who travels in space" },
+        { word: "galaxy", hint: "A system of stars, dust, and gas held together by gravity" },
+        { word: "asteroid", "hint": "A small rocky object that orbits the Sun"},
+        { word: "star", "hint": "A luminous ball of gas that generates energy through nuclear fusion"},
+        { word: "rocket", hint: "A vehicle that travels through space" },
+        { word: "planet", hint: "A celestial body that orbits a star" },
+        { word: "moon", hint: "A natural satellite that orbits a planet" }, 
+        { word: "quasar", hint: "an extremely luminous and distant active galactic nucleus" },
+    ],
+    food: [
+        { word: "pizza", hint: "A popular Italian dish with a round, flat base topped with various ingredients" },
+        { word: "burger", hint: "A sandwich consisting of a cooked patty of ground meat" },
+        { word: "sushi", hint: "A Japanese dish consisting of vinegared rice and various ingredients" },
+        { word: "pasta", hint: "Italian food made from flour, water, and sometimes eggs" },
+        { word: "ice cream", hint: "A frozen dessert made from dairy products and flavored with various ingredients" }
+    ],
+    games: [
+        { word: "chess", hint: "A strategic board game for two players" },
+        { word: "monopoly", hint: "A popular board game where players buy, sell, and trade properties" },
+        { word: "jenga", hint: "A game where players take turns removing one block at a time from a tower and balancing it on top"},
+        { word: "football", hint: "A team sport played with an oval-shaped ball" },
+        { word: "tetris", hint: "A tile-matching puzzle video game" },
+        { word: "scrabble", hint: "A word game where players score points by placing tiles with letters onto a board" }
+        ]
+    };
